cf14867d44275920a538ad6cc727e90d15616335
MDAlviano <mdalviano.21122@gmail.com>
2024-09-27 20:17:51 +0700

    nggawe fungsi deleteAllNotes()

--- app/src/main/java/com/alviano/app/mynotes/fragment/list/ListFragment.kt
+++ app/src/main/java/com/alviano/app/mynotes/fragment/list/ListFragment.kt
@@ -3,8 +3,13 @@ package com.alviano.app.mynotes.fragment.list
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -43,7 +48,32 @@ class listFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
+        setHasOptionsMenu(true)
+
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_delete){
+            deleteAllNotes()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteAllNotes() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes"){ _, _ ->
+            mNoteViewModel.deleteAllNotes()
+            Toast.makeText(requireContext(), "Successfully removed everything", Toast.LENGTH_SHORT).show()
+        }
+        builder.setNegativeButton("No"){ _, _ -> }
+        builder.setTitle("Deleter everything?")
+        builder.setMessage("Are you sure you want to delete everything?")
+        builder.create().show()
     }
 
 }
